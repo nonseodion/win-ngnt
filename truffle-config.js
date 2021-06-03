7c3afc6d95f0d5cbba27ccfea163ed2420b09db0
nonseodion <nonseifebhor@gmail.com>
2021-06-03 13:35:49 +0100

    corrected bsc provider url

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
-          "https://data-seed-prebsc-1-s1.binance.org:8545"
+          "https://data-seed-prebsc-1-s1.binance.org:8545",
         ),
       network_id: 97,
       confirmations: 10,
@@ -54,8 +54,7 @@ module.exports = {
       skipDryRun: true,
     },
     bsc: {
-      provider: () =>
-        new HDWalletProvider(mnemonic, "https://bsc-dataseed1.binance.org"),
+      provider: () => new HDWalletProvider(mnemonic, "https://bsc-dataseed.binance.org"),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
